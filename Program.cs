a6d62f3ab2967e8feecfa54a097ae830ca977f22
chakra473 <91408040+chakra473@users.noreply.github.com>
2022-03-20 13:03:51 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,25 +9,6 @@ namespace list
         {
             AddressBook addressBook = new AddressBook();
             addressBook.Display();
-            Person person = new Person();
-            Console.WriteLine("enter your name");
-            string name=Console.ReadLine();
-            Console.WriteLine("enter your city ");
-            string city = Console.ReadLine();
-            Console.WriteLine("enter your phone number");
-            long phoneNumber=long.Parse(Console.ReadLine());
-            Console.WriteLine("enter your postal code");
-            long postalCode=long.Parse(Console.ReadLine());
-            person.Name = name;
-            person.City = city;
-            person.PhoneNumber = phoneNumber;
-            person.PostalCode = postalCode;
-            addressBook.AddPerson(person);
-            addressBook.Display();
-
-
-
-          
         }
     }
 }
