6482994bcbfd9eca9bac365ae26e79e603adf018
chakravarthy <m.chakra473@gmail.com>
2022-03-20 13:45:48 +0530

    added [add a new contact to address book] to uc2

--- Program.cs
+++ Program.cs
@@ -10,24 +10,49 @@ namespace list
             AddressBook addressBook = new AddressBook();
             addressBook.Display();
             Person person = new Person();
-            Console.WriteLine("enter your name");
-            string name=Console.ReadLine();
-            Console.WriteLine("enter your city ");
-            string city = Console.ReadLine();
-            Console.WriteLine("enter your phone number");
-            long phoneNumber=long.Parse(Console.ReadLine());
-            Console.WriteLine("enter your postal code");
-            long postalCode=long.Parse(Console.ReadLine());
-            person.Name = name;
-            person.City = city;
-            person.PhoneNumber = phoneNumber;
-            person.PostalCode = postalCode;
-            addressBook.AddPerson(person);
+           
             addressBook.Display();
+            
+            while (true)
+            {
+                Console.WriteLine("press:D to delete the contact \npress:E to edit the contact \npress:A to add the contact \npress:s to display the details ");
+                char c = char.Parse(Console.ReadLine());
+                if ((c == 'D') || (c == 'd'))
+                {
+                    Console.WriteLine("enter name to delete");
+                    string dname = Console.ReadLine();
+                    person.Name = dname;
+                    addressBook.RemovePerson(dname);
+                }
+                else if ((c=='A') || (c=='a'))
+                {
+                    Console.WriteLine("enter your name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("enter your city ");
+                    string city = Console.ReadLine();
+                    Console.WriteLine("enter your phone number");
+                    long phoneNumber = long.Parse(Console.ReadLine());
+                    Console.WriteLine("enter your postal code");
+                    long postalCode = long.Parse(Console.ReadLine());
+                    person.Name = name;
+                    person.City = city;
+                    person.PhoneNumber = phoneNumber;
+                    person.PostalCode = postalCode;
+                    addressBook.AddPerson(person);
+                }
+                else if((c=='S')||(c=='s'))
+                {
+                    addressBook.Display();
+                }
+                else
+                {
+                    break;
+                }
+            }
 
 
 
-          
+
         }
     }
 }
